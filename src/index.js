e8a1e4a86580d92f5e8bdcdcdfe1c8d2e1e25810
Wojciech Król <wk20981@gmail.com>
2019-02-03 19:38:51 +0100

    Remove ReduxComposer, restore default reducer combining

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import reducers from "./store/reducers";
 
 import "./index.css";
 
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,11 +1,8 @@
 import { combineReducers } from "redux";
-import ReduxComposer from "../utils/ReduxComposer";
 import todos from "./todos/reducer";
 import filter from "./filter/reducer";
 
-const composer = new ReduxComposer();
-
-composer.add(todos);
-composer.add(filter);
-
-export default combineReducers(composer.all());
+export default combineReducers({
+  todos,
+  filter,
+});
